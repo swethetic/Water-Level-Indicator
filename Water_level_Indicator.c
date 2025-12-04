977272cafe33b686b503ba8a3609b9dcfd8ac5c2
swethetic <144234049+swethetic@users.noreply.github.com>
2025-12-04 23:49:55 +0530

    Add files via upload

---
+++ Water_level_Indicator.c
@@ -0,0 +1,27 @@
+8.  #include <stdio.h>
+
+int main() {
+    float level;
+    printf("Enter tank level: ");
+    scanf("%f", &level);
+    if(level < 0) {
+        printf("Error: Invalid\n");
+    }
+    else if(level <= 9) {
+        printf("LOW+ ALERT!!!+ FILL\n");
+    }
+    else if(level <= 29) {
+        printf("LOW+FILL\n");
+    }
+    else if(level <= 70) {
+        printf("MEDIUM\n");
+    }
+    else if(level <= 100) {
+        printf("HIGH\n");
+    }
+    else {
+        printf("Error: Invalid\n");
+    }
+
+    return 0;
+}
